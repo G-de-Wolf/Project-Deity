b2e178dd5c846f841cb9156d5ab5ad4f4514dd85
G-de-Wolf <guidovic2@hotmail.com>
2017-01-30 14:02:02 +0100

    Project Deity
    First commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 10f;
+
+    Vector3 movement;           //Vector for direction of player movement
+    //Animator anim;
+    Rigidbody playerRigidbody;  //Player's rigidbody
+    int floorMask;              //Floor layer
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Move(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+
+    }
+}
