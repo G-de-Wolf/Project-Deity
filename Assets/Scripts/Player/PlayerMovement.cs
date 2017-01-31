cf9a42713fabcc6c6179df021ceaae15a24778cd
G-de-Wolf <guidovic2@hotmail.com>
2017-01-31 13:09:16 +0100

    Player movement
    Moving the player in the direction the player is looking at

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,32 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float speed = 12f;
+    public float speed = 10f;
 
-    Vector3 movement;           //Vector for direction of player movement
-    //Animator anim;
-    Rigidbody playerRigidbody;  //Player's rigidbody
-    int floorMask;              //Floor layer
+    Vector3 movement;           
+    Animator anim;
+    Rigidbody playerRigidbody;  
 
-    void Awake()
-    {
-        floorMask = LayerMask.GetMask("Floor");
+    void Awake() {
+        //anim = GetComponent<Animator>();
         playerRigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update()
-    {
+    void Update() {
         float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float v = Input.GetAxisRaw("Vertical");
 
         Move(h, v);
+        //Animating(h, v);
     }
 
-    void Move(float h, float v)
-    {
-        movement.Set(h, 0f, v);
-        movement = movement.normalized * speed * Time.deltaTime;
-        playerRigidbody.MovePosition(transform.position + movement);
+    void Move(float h, float v) {
+        float horizontal = h * speed * Time.deltaTime;
+        transform.Translate(horizontal, 0, 0);
 
+        float vertical = v * speed * Time.deltaTime;
+        transform.Translate(0, 0, vertical);
+    }
+
+    void Animating(float h, float v) {
+        bool walking = h != 0f || v != 0f;
+
+        anim.SetBool("IsWalking", walking);
     }
 }
